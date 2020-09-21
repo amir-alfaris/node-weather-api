5b861974aa828fe423c8f3b2808836930f6f5ad4
Amir Ayub <amirayubx@gmail.com>
2020-09-21 20:36:52 +0300

    init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    let url = `https://www.metaweather.com/api/location/search/?query=${req.body.city}`;
+    axios.get(url)
+        .then(({data}) => res.json(data))
+        .catch((err) => res.json(err));
+});
+
+app.listen(port, () => {
+    console.log(`Weather app listening at http://localhost:${port}`);
+});
